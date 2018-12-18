68d7740f0d747f8c7119ed8f9c21f7408d8ecc90
Robert <rowbax@gmail.com>
2018-12-18 23:54:12 +0100

    z2

--- com/company/MyClass1.java
+++ com/company/MyClass1.java
@@ -5,5 +5,17 @@ import java.util.Objects;
 public class MyClass1 {
     private String str;
 
+    public String getStr() {
+        return str;
     }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    Integer int;
+
+
 }
+
+
