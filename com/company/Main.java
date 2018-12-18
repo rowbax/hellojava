0f2c20e191361ad03c12a1fb016f5cb142767110
Robert <rowbax@gmail.com>
2018-12-18 22:25:54 +0100

    first commit

---
+++ com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str2 = String.format("My answer is %d", 4765734);
+        System.out.println(str2);
+    }
+}
