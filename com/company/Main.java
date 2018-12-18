2948799bbcb4ae3728f9772c1ba9e90a313e732d
Robert <rowbax@gmail.com>
2018-12-18 22:34:06 +0100

    zmaina

--- com/company/Main.java
+++ com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String str1 = String.format("My answer is %d", 4765734);
         String str2 = String.format("My answer is %d", 4765734);
+        String str3 = String.format("My answer is %d", 4765734);
         System.out.println(str2);
 
     }
