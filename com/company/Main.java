db6cda16652a38f04c5227ec4a3269882fc86eec
Robert <rowbax@gmail.com>
2018-12-19 00:18:56 +0100

    zm3

--- com/company/Main.java
+++ com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        String str1 = String.format("My answer is %d", 4765734);
         String str2 = String.format("My answer is %d", 4765734);
         System.out.println(str2);
 
